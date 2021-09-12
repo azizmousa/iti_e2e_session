1265deac4249833f0aa600f223b32f07171adc8d
azizmousa <azizmousa1010@gmail.com>
2021-09-12 19:40:23 +0200

    initial-commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='learn how to build end to end project in data science',
+    author='azizmousa',
+    license='',
+)
